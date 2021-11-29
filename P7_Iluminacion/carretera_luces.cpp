933afc68bfb3c219e540539493d9b58f6b392f70
iranz15 <82667970+iranz15@users.noreply.github.com>
2021-11-29 13:13:55 +0100

    Avances Modo Nocturno

--- P7_Iluminacion/carretera_luces.cpp
+++ P7_Iluminacion/carretera_luces.cpp
@@ -1,12 +1,12 @@
 /*!
-	\file		Reloj_3D.cpp
-	\brief		Practica 5 Reloj_3D_Digital
+	\file		carretera_luces.cpp
+	\brief		Practica 7 Iluminacion
 	\author		Jorge Iranzo' <jorirsan@upv.es>
 	\date		2021-2022
  */
 
 
-#define PROYECTO "Conducción nocturna - Jorge Iranzo"
+#define PROYECTO " - Jorge Iranzo"
  //#define _USE_MATH_DEFINES
 
 #include <iostream>		
@@ -37,19 +37,22 @@ static float Z = -2;
 
 //Variables de generacion de la carretera
 static float amplitud = 4;
-static float periodo = 50 ;
+static float periodo = 50;
 static int nQuads = 50;
 static int ancho = 4;
-static int distancia = 5;
+static int distancia = 1;
 
 //Variables de control de camara
 static float ratioGiro = 5;
 static float angulo = 90.0; //IMPORTANTE: Se genera la carretera hacia el eje +Z . Inicialmente desde 0.0.0 la tangente forma 90 grados con +X.
 static float velocidad = 0.0;
-static int mirar  =  1;		//Constante para controlar la distancia entre la camara y el punto que esta mirando. Se ha escogido de forma arbitraria
+static int mirar = 1;		//Constante para controlar la distancia entre la camara y el punto que esta mirando. Se ha escogido de forma arbitraria
 
+
+//Flags de control
 //Vista normal = 1/ Vista pajaro = 0
 static int vista = 1;
+static int noche = 0;
 
 
 /*		Vista de pajaro
@@ -70,7 +73,8 @@ X+    <---------|-------->     -X
 void init()
 {
 	//Inicializacion de metodos que cargan lista para luego poder llamarlas en display o otros metodos
-	glClearColor(1.0, 1.0, 1.0, 1.0); // Fondo Blanco
+	 // Fondo Blanco
+
 	glEnable(GL_DEPTH_TEST);		//Test de profundidad
 
 }
@@ -79,7 +83,7 @@ void init()
 
 float funcionCarretera(int punto) {
 
-	return amplitud* sin(punto * 2*PI / periodo);
+	return amplitud * sin(punto * 2 * PI / periodo);
 
 }
 
@@ -93,20 +97,20 @@ float normal(float d) {
 
 float derivada(int punto) {
 
-	return( ( (2 * PI * amplitud) / periodo )* cos( punto * 2 * PI / periodo));
+	return(((2 * PI * amplitud) / periodo) * cos(punto * 2 * PI / periodo));
 }
 
 
 void circuito() {
 
-	
 
-	glColor3f(0.0, 0.0, 0.0);
+
+	glColor3f(0, 0.3, 0.3);
 	int l = ancho / 2;
 	for (int i = 1; i <= nQuads; i++) {
 
 		int punto = Z + (i - 1) * distancia;
-		int siguiente = punto+distancia;
+		int siguiente = punto + distancia;
 
 		float x = funcionCarretera(punto);
 		float d = derivada(punto);
@@ -122,17 +126,22 @@ void circuito() {
 		GLfloat v1[3] = { x1 - (n1 * l), 0.0 , siguiente - (-1 * d1 * n1 * l) };
 		GLfloat v2[3] = { x1 + (n1 * l) , 0.0 , siguiente + (-1 * d1 * n1 * l) };
 
-
-		glPolygonMode(GL_FRONT, GL_LINE);
-
+		
 		quad(v0, v1, v2, v3, 10, 5);
 
 	}
-	
-	
+
+
 }
 
+void luces(){
 
+
+
+
+
+
+}
 void display()
 {
 
@@ -140,24 +149,28 @@ void display()
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	if (modoVisu == ALAMBRICO) 
+	if (noche) glClearColor(0.0, 0.0, 0.0, 0.0); //Fondo negro
+	else glClearColor(1.0, 1.0, 1.0, 1.0);		//Fondo blanco
+	
+	if (modoVisu == ALAMBRICO)
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	
-	else 
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-	
-	if (vista) gluLookAt(X, Y, Z, X + mirar * cos(angulo * PI / 180), Y, Z + mirar * sin(angulo * PI / 180), 0, vista, 1-vista);
-	
-	else gluLookAt(X+20, Y+50, Z, X + mirar * cos(angulo * PI / 180), Y, Z + mirar * sin(angulo * PI / 180), 0, vista, 1 - vista);
 
-	
+	else
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	if (vista) gluLookAt(X, Y, Z, X + mirar * cos(angulo * PI / 180), Y, Z + mirar * sin(angulo * PI / 180), 0, vista, 1 - vista);
+
+	else gluLookAt(X + 20, Y + 50, Z, X + mirar * cos(angulo * PI / 180), Y, Z + mirar * sin(angulo * PI / 180), 0, vista, 1 - vista);
+
+
 
 	ejes();
-	glColor3f(1, 0.3, 0.3);
+	
 	//glutSolidCone(0.4, 2, 30, 5);
+	luces();
 	circuito();
 
-	
+
 
 	glutSwapBuffers();
 
@@ -196,34 +209,40 @@ void onTimer(int valor) {
 	tSegundos = timeinfo->tm_sec;
 	Z += sin(angulo * PI / 180) * velocidad * tiempo_transcurrido / 1000.0f;
 	X += cos(angulo * PI / 180) * velocidad * tiempo_transcurrido / 1000.0f;
-	
+
 	stringstream titulo;
 	titulo << fixed;
 	titulo.precision(1); // De esta forma no aparecen errores de coma flotante en la ventana
-	titulo << velocidad << "m/s"; 
+	titulo << velocidad << "m/s";
 	glutSetWindowTitle(titulo.str().c_str());
 
 	glutTimerFunc(1000 / tasaFPS, onTimer, tasaFPS);
 	glutPostRedisplay();
 }
 
-void onKey(unsigned char tecla, int x, int y) { 
+void onKey(unsigned char tecla, int x, int y) {
 
 	switch (tecla) {
 
-		case 'a':
-			modoVisu = ALAMBRICO;
-			break;
-		case 's':
-			modoVisu = SOLIDO;
-			break;
-		case 'c':
-			if (vista)
+	case 'a':
+		modoVisu = ALAMBRICO;
+		break;
+	case 's':
+		modoVisu = SOLIDO;
+		break;
+	case 'c':
+		if (vista)
 			vista = 0;
-			else vista = 1;
-			break;
-		case 27:
-			exit(0);
+		else vista = 1;
+		break;
+	case 'n':
+		if (noche)
+			noche = 0;
+		else noche = 1;
+
+		break;
+	case 27:
+		exit(0);
 
 	}
 
@@ -232,26 +251,26 @@ void onKey(unsigned char tecla, int x, int y) {
 
 
 
-void onSpecialKey(int specialKey, int x, int y) { 
+void onSpecialKey(int specialKey, int x, int y) {
 
 	switch (specialKey) {
-		
 
 
-		case GLUT_KEY_UP:
-			velocidad += 0.1;
-			break;
-		case GLUT_KEY_DOWN:
-			if (velocidad <= 0.1) //Para evitar 
-				velocidad = 0;
-			else velocidad -= 0.1;
-			break;
-		case GLUT_KEY_LEFT:
-			angulo -= ratioGiro;
-			break;
-		case GLUT_KEY_RIGHT:
-			angulo += ratioGiro;
-			break;
+
+	case GLUT_KEY_UP:
+		velocidad += 0.1;
+		break;
+	case GLUT_KEY_DOWN:
+		if (velocidad <= 0.1) //Para evitar 
+			velocidad = 0;
+		else velocidad -= 0.1;
+		break;
+	case GLUT_KEY_LEFT:
+		angulo -= ratioGiro;
+		break;
+	case GLUT_KEY_RIGHT:
+		angulo += ratioGiro;
+		break;
 
 
 	}
